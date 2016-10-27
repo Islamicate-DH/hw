17cea8921fa0e7c9be3e59c1ede49b639552ac7c
J. R. Schmid <jrs+git@weitnahbei.de>
2016-10-27 23:34:20 +0200

    Construction site opened:
    - removing all scraping code except for extract_number_of_pages
    - downloading HTML into files right away without any modification
    - removed code will either become second script or might be reintroduced
      here in a different form; haven't decided yet. Things are a bit
      intertwined because of the way altafsir.com is programmed.

--- tafaseer_topic_group/scrape_corpus.R
+++ tafaseer_topic_group/scrape_corpus.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 # Only runs on R >= 3.3.1!
 # Requires: urltools, rvest, XML, magrittr, stringr
+# Remember: every line of code one liability!
 
 for (lib in c('urltools', 'rvest', 'stringr')) {
   suppressPackageStartupMessages(library(lib, character.only = TRUE))
@@ -16,7 +17,7 @@ number_of_suwwar  = 114
 number_of_tafaseer_per_madrasa = c(8, 20, 10, 2, 7, 7, 4, 3, 5, 2) 
 number_of_ayaat_per_sura       = c(7, 286, 200, 176, 120, 165, 206, 75, 129, 109, 123, 111, 43, 52, 99, 128, 111, 110, 98, 135, 112, 78, 118, 64, 77, 227, 93, 88, 69, 60, 34, 30, 73, 54, 45, 83, 182, 88, 75, 85, 54, 53, 89, 59, 37, 35, 38, 29, 18, 45, 60, 49, 62, 55, 78, 96, 29, 22, 24, 13, 14, 11, 11, 18, 12, 12, 30, 52, 52, 44, 28, 28, 20, 56, 40, 31, 50, 40, 46, 42, 29, 19, 36, 25, 22, 17, 19, 26, 30, 20, 15, 21, 11, 8, 8, 19, 5, 8, 8, 11, 11, 8, 3, 9, 5, 4, 7, 3, 6, 3, 5, 4, 5, 6)
 
-download_all_tafaseer <- function(url, pos = c(1,1,1,1))
+download_all <- function(url, pos = c(1,1,1,1))
 {
   pos_set = FALSE
   # The mother of all nested loops. Makes Ross' and Robert's hearts cringe,
@@ -37,39 +38,26 @@ download_all_tafaseer <- function(url, pos = c(1,1,1,1))
       for (sura in 1:number_of_suwwar) {
         if (!pos_set && sura < pos[3]) {next}
         for (ayah in 1:number_of_ayaat_per_sura[sura]) {
-          if (!pos_set) {if (ayah < pos[4]) {next} else {pos_set = TRUE}}
-          url = param_set(url, 'tMadhNo',   madrasa)
-          url = param_set(url, 'tSoraNo',   sura)
-          url = param_set(url, 'tTafsirNo', tafsir)
-          url = param_set(url, 'tAyahNo',   ayah)
+          if (!pos_set) {if (ayah < pos[4]) {next} else {pos_set = TRUE}}          
           delim = rep('–', 115)
           message(
             c("\033[2J","\033[0;0H"), delim, '\n Working…\n', delim,
             sprintf('\n Madrasa:\t%s/%s | ', madrasa, number_of_madaris),
             sprintf('Tafsir:\t%s/%s | ', tafsir, number_of_tafaseer_per_madrasa[madrasa]),
-            sprintf('Sura:\t%s/%s | ', sura, number_of_suwwar),
+            sprintf('sura:\t%s/%s | ', sura, number_of_suwwar),
             sprintf('Ayah:\t%s/%s\n', ayah, number_of_ayaat_per_sura[sura]),
-            delim, '\n', url)
-          # Page 1
-          raw_html = read_html(url)
-          n = extract_number_of_pages(raw_html)
-          text.meta = extract_meta(raw_html) # Returns vector: 1=title,2=author,3=year
-          text.ayah   = paste('<article class="ayah">', extract_ayah(raw_html), '</article>', sep='')
-          text.tafsir = paste('<article class="tafsir"><p>', extract_tafsir(raw_html), '</p>', sep='')
-          # Pages 2-n
+            delim, '\n', url)          
+          page = 1
+          file = download(url, sura, ayah, madrasa, tafsir, page)
+          parsed_html = read_html(file)
+          n = extract_number_of_pages(parsed_html)
           if (n > 1) {
             for (page in 2:n) {
-              url = param_set(url, 'Page', page)
-              message('\t(Page ', page, ')')
-              raw_html = read_html(url)
-              text.tafsir = paste(text.tafsir, '<p>', extract_tafsir(raw_html), '</p>', sep='')
+              download(url, sura, ayah, madrasa, tafsir, page)
             }
           }
-          text.tafsir = paste(text.tafsir, '</article>', sep='')
-          message('\tComplete. Around ', str_count(text.tafsir, '\\S+'), ' words pasted.')
-          save_to_disk(c(madrasa, tafsir, sura, ayah), text.meta, text.ayah, text.tafsir)
           url = param_set(url, 'Page', 1) # Reset page number!
-          sleep(1) # Sleep up to 1 second so we're not seen as a threat.
+          # sleep(1) # Sleep up to 1 second so we're not seen as a threat.
         }
       } 
     } 
@@ -83,25 +71,62 @@ sleep <- function(s)
   proc.time() - t0
 }
 
-save_to_disk <- function(pos, meta, ayah, tafsir)
+download <- function(url, sura, ayah, madrasa, tafsir, page)
 {
-  # Ayah text first
-  path = file.path('corpus', 'altafsir.com', 'ayaat')
-  file = file.path(path, sprintf('%s:%s.txt', pos[3], pos[4]))
+  url = param_set(url, 'tMadhNo',   madrasa)
+  url = param_set(url, 'tSoraNo',   sura)
+  url = param_set(url, 'tTafsirNo', tafsir)
+  url = param_set(url, 'tAyahNo',   ayah)
+  url = param_set(url, 'Page',      page)
+  path = file.path('corpus', 'altafsir.com', 'raw',
+         sprintf('quran_%s,%s-school_%s-tafsir_%s',
+         sura, ayah, madrasa, tafsir)) # Logical order
+  file = file.path(path, sprintf('page_%s.html', page))
   dir.create(path, showWarnings = FALSE, recursive = TRUE)
-  write(ayah, file)
-  message('\tSaved ayah to ', file)
-  # Now the tafseer text
-  path = file.path('corpus', 'altafsir.com', 'tafaseer')
-  file = file.path(path, sprintf('%s:%s - %s (%s).txt', pos[3], pos[4], pos[2], pos[3]))
-  dir.create(path, showWarnings = FALSE, recursive = TRUE)
-  write(tafsir, file)
-  message('\tSaved tafsir to ', file)
+  download.file(url, file, quiet=TRUE, cacheOK=FALSE)
+  message(sprintf('\tSaved page %s to %s', page, file))
   # Position marker so we can pick up where we left off
-  file = file.path('corpus', 'altafsir.com', 'scraper_pos.dat')
-  pos  = sprintf('%s,%s,%s,%s', pos[1], pos[2], pos[3], pos[4])
+  file = file.path('corpus', 'altafsir.com', 'raw', 'scraper_pos.dat')
+  pos  = sprintf('%s,%s,%s,%s', madrasa, tafsir, sura, ayah) # Website's order
   write(pos, file)
+  # Contents are required by first caller!
+  return(file)
 }
+
+#save_to_disk <- function(pos, data)
+#{
+#  path = file.path('corpus', 'altafsir.com', 'raw',
+#         sprintf('quran_%s,%s-school_%s-tafsir_%s',
+#         pos[1], pos[2], pos[3], pos[4])) # Logical order!
+#  file = file.path(path, sprintf('page_%s.html', pos[5]))
+#  dir.create(path, showWarnings = FALSE, recursive = TRUE)
+#  write(data, file)
+#  message('\tSaved page to ', file)
+#  # Position marker so we can pick up where we left off
+#  file = file.path('corpus', 'altafsir.com', 'raw', 'scraper_pos.dat')
+#  pos  = sprintf('%s,%s,%s,%s', pos[3], pos[4], pos[1], pos[2]) # Website's order!
+#  write(pos, file)
+#}
+
+#save_to_disk <- function(pos, meta, ayah, tafsir)
+#{
+#  # Ayah text first
+#  path = file.path('corpus', 'altafsir.com', 'ayaat')
+#  file = file.path(path, sprintf('%s:%s.txt', pos[3], pos[4]))
+#  dir.create(path, showWarnings = FALSE, recursive = TRUE)
+#  write(ayah, file)
+#  message('\tSaved ayah to ', file)
+#  # Now the tafsir text
+#  path = file.path('corpus', 'altafsir.com', 'tafaseer')
+#  file = file.path(path, sprintf('%s:%s - %s (%s).txt', pos[3], pos[4], pos[2], pos[3]))
+#  dir.create(path, showWarnings = FALSE, recursive = TRUE)
+#  write(tafsir, file)
+#  message('\tSaved tafsir to ', file)
+#  # Position marker so we can pick up where we left off
+#  file = file.path('corpus', 'altafsir.com', 'scraper_pos.dat')
+#  pos  = sprintf('%s,%s,%s,%s', pos[1], pos[2], pos[3], pos[4])
+#  write(pos, file)
+#}
 
 # Notes for scraping altafsir.com
 # ===============================
@@ -137,45 +162,45 @@ extract_number_of_pages <- function(raw_html)
   return(n)
 }
 
-extract_meta <- function(raw_html)
-{
-  meta <- raw_html %>%
-    html_nodes('#DispFrame .TextResultArabic') %>%
-    html_text() %>%
-    head(1) # Meaning "nth-child(1)" - not sure if that'd work...
-  regex = "\\*\\s*(?<title>.*?) ?\\/ ?(?<author>\\s?.*?)\\s?\\(\\D*(?<year>\\d{3,4}).*\\)"
-  meta = unlist(regmatches(meta, regexec(regex, meta, perl=TRUE)))
-  message('\tTitle: ', meta[2])
-  message('\tAuthor: ', meta[3])
-  message('\tYear: ', meta[4])
-  return(meta[2:4])
-}
+#extract_meta <- function(raw_html)
+#{
+#  meta <- raw_html %>%
+#    html_nodes('#DispFrame .TextResultArabic') %>%
+#    html_text() %>%
+#    head(1) # Meaning "nth-child(1)" - not sure if that'd work...
+#  regex = "\\*\\s*(?<title>.*?) ?\\/ ?(?<author>\\s?.*?)\\s?\\(\\D*(?<year>\\d{3,4}).*\\)"
+#  meta = unlist(regmatches(meta, regexec(regex, meta, perl=TRUE)))
+#  message('\tTitle: ', meta[2])
+#  message('\tAuthor: ', meta[3])
+#  message('\tYear: ', meta[4])
+#  return(meta[2:4])
+#}
 
-extract_ayah <- function(raw_html)
-{
-  ayah <- raw_html %>%
-    html_nodes('#AyahText .TextAyah') %>%
-    html_text()
-  message('\tAyah: ', ayah)
-  return(trimws(ayah))
-}
+#extract_ayah <- function(raw_html)
+#{
+#  ayah <- raw_html %>%
+#    html_nodes('#AyahText .TextAyah') %>%
+#    html_text()
+#  message('\tAyah: ', ayah)
+#  return(trimws(ayah))
+#}
 
-extract_tafsir <- function(raw_html)
-{
-  tafsir <- raw_html %>%
-    html_nodes('#SearchResults font font') %>%
-    html_text()
-  tafsir = paste(tafsir, collapse=' ')
-  message('\tTafsir: ', paste(strtrim(tafsir, 35), 
-          '… (~', str_count(tafsir, '\\S+'), 'words total)'))
-  return(trimws(tafsir))
-}
+#extract_tafsir <- function(raw_html)
+#{
+#  tafsir <- raw_html %>%
+#    html_nodes('#SearchResults font font') %>%
+#    html_text()
+#  tafsir = paste(tafsir, collapse=' ')
+#  message('\tTafsir: ', paste(strtrim(tafsir, 35), 
+#          '… (~', str_count(tafsir, '\\S+'), 'words total)'))
+#  return(trimws(tafsir))
+#}
 
 # Let's rock'n'roll!
-file = file.path('corpus', 'altafsir.com', 'scraper_pos.dat')
+file = file.path('corpus', 'altafsir.com', 'raw', 'scraper_pos.dat')
 if (file.exists(file)) {
   pos = unlist(read.csv(file, header=FALSE))
-  download_all_tafaseer(default_url, pos)
+  download_all(default_url, pos)
 } else {
-  download_all_tafaseer(default_url)
+  download_all(default_url)
 }
