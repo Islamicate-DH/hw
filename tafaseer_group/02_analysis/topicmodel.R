c120c66882ec95e6cb57ddab040be0b630323dbf
J. R. Schmid <jrs+git@weitnahbei.de>
2016-12-09 14:56:46 +0100

    Making it finally work in a workable manner. Man, this TM got on my nerves!!!

--- tafaseer_group/02_analysis/topicmodel.R
+++ tafaseer_group/02_analysis/topicmodel.R
@@ -1,15 +1,30 @@
 #!/usr/bin/env Rscript
 
 # R packages
-for (lib in c('tm', 'topicmodels', 'lda', 'LDAvis')) {
+for (lib in c('tm', 'topicmodels', 'lda', 'LDAvis', 'mgcv')) {
   suppressPackageStartupMessages(library(lib, character.only = TRUE))
 }
 
 # Home-made library functions
 source(file.path('..', 'lib', 'clean_arabic_string.r'))
 
-# TODO: Think about first implementing
-# https://cran.r-project.org/web/packages/ldatuning/vignettes/topics.html
+save_as_rds <- function(savedir, datafiles)
+{
+  path = sprintf("%s/data.rds", savedir)
+  if (file.exists(path)) {
+		message('loading data from rds...')
+    load(path) # this (re)creates the data object
+  } else {
+    message('saving data as rds...')
+    txt <- lapply(datafiles, readLines)
+    names = gsub(savedir, '', datafiles)
+    data = setNames(txt, names)
+    data = sapply(data, function(x) paste(x, collapse = ' '))
+    save(data, file=path, compress='xz')
+  }
+  return(data)
+}
+
 params = list(
   burnin  = 4000,
   iter    = 2000,
@@ -22,195 +37,109 @@ params = list(
   verbose = 2000  # Show progress every x iterations
 )
 
-build_dtm <- function(file)
+fit_model <- function(savedir, data, stopwords)
 {
-  message('Reading in files...')
-  file = lapply(file, readLines)
-  message('Building corpus...')
-  corpus = Corpus(VectorSource(file))
-  message('Building DT matrix...')
-  DocumentTermMatrix(corpus)
+  path = sprintf("%s/fit.rds", savedir)
+  if (file.exists(path)) {
+    message('loading fit from rds...')
+    fit <- readRDS(path)
+  } else {
+    # should already be done, so not doing it again (compute-intensive)
+    # tafaseer = clean_arabic_string(tafaseer)
+    message('tokenizing...')
+      doc.list = strsplit(data, '[[:space:]]+')
+      message('computing table of terms...')
+      term.table = table(unlist(doc.list))
+      term.table = sort(term.table, decreasing=TRUE)
+    message('removing superfluous terms...')
+      del = names(term.table) %in% stopwords | term.table < 5
+      term.table = term.table[!del]
+      vocab = names(term.table)
+    message('converting to lda input format...')
+      get.terms = function(x) {
+        index = match(x, vocab)
+        index = index[!is.na(index)]
+        rbind(as.integer(index - 1), as.integer(rep(1, length(index))))}
+      documents = lapply(doc.list, get.terms)
+    message('some statistics related to the data set:')
+		num_tokens = sapply(documents, function(x) sum(x[2, ]))
+		term_freqs = as.integer(term.table)
+		message(sprintf('no. of docs: %i\nno. of terms: %i\nno. of tokens: %s\ntotal no. of tokens: %i\ntop 10 term frequencies: %s',
+			length(documents),
+      length(vocab),
+      paste(num_tokens, collapse=', '),
+      sum(num_tokens),
+      paste(term_freqs[(1:10)], collapse=', ')))
+    message('fitting the model (go grab a nap)...')
+      # TODO: Think about first implementing
+      # https://cran.r-project.org/web/packages/ldatuning/vignettes/topics.html
+      # TODO: find out where they take the seed value(s) from??
+      # set.seed(list(2003, 5, 63, 100001, 765))
+      set.seed(357)
+      alpha = 0.02
+      eta   = 0.02
+      fit <- lda.collapsed.gibbs.sampler(
+        documents      = documents,
+        vocab          = vocab,
+        K              = 20,
+        num.iterations = 5000,
+        initial        = NULL,
+        burnin         = 0,
+        # TODO: look into what these would really change...
+        # K              = 120,
+        # num.iterations = 2000,
+        # TODO: find out how to specify these with sampler()
+        # burnin         = 4000,
+        # thin=500,
+        # nstart=5,
+        # verbose=2000, # milliseconds
+        # best = TRUE,
+        alpha = alpha,
+        eta = eta,
+        compute.log.likelihood=TRUE)
+      # needed for building the json later on
+      fit$num_tokens = num_tokens
+      fit$term_freqs = term_freqs
+      fit$alpha = alpha
+      fit$eta = eta
+      fit$vocab_list = vocab
+    saveRDS(fit, path)
+  }
+  return(fit)
 }
 
-crunch_lda <- function(dtm, params)
+build_v11n <- function(savedir, fit)
 {
-  message('Crunching LDA function...')
-  LDA(
-    dtm,
-    params$k,
-    # Who is he and why do we like him?
-    # Answer Leeroy Jethro is of course our hero. In this case,
-    # because he allows running some LDA steps on multiple processors
-    # and also because he has lower memory requirements than previous
-    # methods. MEMORY USAGE STILL RISES WITH CORPUS SIZE, THOUGH!!!
-    # (Cf. https://cran.r-project.org/web/packages/topicmodels/vignettes/topicmodels.pdf)
-    method='Gibbs',
-    control=params
-  )
+  message('building json...')
+	json = createJSON(
+		phi            = t(apply(t(fit$topics) + fit$eta, 2, function(x) x/sum(x))),
+    theta          = t(apply(fit$document_sums + fit$alpha, 2, function(x) x/sum(x))),
+    doc.length     = fit$num_tokens,
+    vocab          = fit$vocab_list,
+    term.frequency = fit$term_freqs)
+  message('writing visualization data...')
+	serVis(json, out.dir=sprintf('%s/html', savedir), open.browser=FALSE)
 }
 
-write_topics <- function(src, lda, k)
-{
-  message('Building topics from LDA results...')
-  topics = as.matrix(topics(lda))
-  write.csv(
-    topics,
-    file=sprintf('data/%s_LDAGibbs-%i-Docs2Topics.csv',src,k)
-  )
-}
-
-write_topTerms <- function(src, lda, n, k)
-{
-  message(sprintf('Calculating top %i topics from LDA results...',k))
-  terms = as.matrix(terms(lda, n))
-  write.csv(
-    terms,
-    file=sprintf('data/%s_LDAGibbs-%i-Topics2Terms.csv',src,k)
-  )
-}
-
-write_probabilities <- function(src, lda, k)
-{
-  message('Calculating topic probabilities from LDA results...')
-  probabs = as.data.frame(lda@gamma)
-  write.csv(
-    probabs,
-    file=sprintf('data/%s_LDAGibbs-%i-TopicProbabilities.csv',src,k)
-  )
-}
-
-write_relativeImportances <- function(src, dtm, probabs, k)
-{
-  message('Finding relative importances between topics 1/2, 2/3...')
-  one2two = lapply(1:nrow(dtm), function(x) {
-    sort(probabs[x,])[k-0] / sort(probabs[x,])[k-1]
-  })
-  two2three = lapply(1:nrow(dtm), function(x) {
-    sort(probabs[x,])[k-1] / sort(probabs[x,])[k-2]
-  })
-  write.csv(
-    one2two,
-    file=sprintf('data/%s_LDAGibbs-%i-TopicOneImportance2Two.csv',src,k)
-  )
-  write.csv(
-    two2three,
-    file=sprintf('data/%s_LDAGibbs-%i-TopicTwoImportance2Three.csv',src,k)
-  )
-}
-
-# Ran this once, not used anymore now
-convert_txt_to_rds <- function(files)
-{
-  txt <- lapply(files, readLines)
-  names <- gsub('../../corpora/altafsir_com/selection', '', files)
-  tafaseer = setNames(txt, names)
-  tafaseer = sapply(tafaseer, function(x) paste(x, collapse = ' '))
-  save(tafaseer, file='../../corpora/altafsir_com/selection/tafaseer.rds', compress='xz')
-}
-
-# The old thing, not used anymore now
-run_lda <- function(file)
+run_tm <- function(savedir, stopwordfile, datafiles)
 {
   t0 = proc.time()
-  message(sprintf('Execution starts at %.3fms', t0[1]))
-  dtm = build_dtm(file)
-  lda_results = crunch_lda(dtm, params)
-  # What's with all the parentheses, R?!
-  basename = strsplit(basename(file), '\\.')[[1]][1]
-  write_topics(basename, lda_results, params$k)
-  write_topTerms(basename, lda_results, 6, params$k)
-  probabs = write_probabilities(basename, lda_results, params$k)
-  write_relativeImportances(basename, dtm, probabs, params$k)
-  message(
-    sprintf('Success after %.2f mins',
-    (proc.time() - t0)[3] / 60)
-  )
-}
+  message('execution starts at 0.00 mins')
 
-t <- function(t0)
-{
-  (proc.time() - t0)[3] / 60
-}
+  # basically going by http://cpsievert.github.io/ldavis/reviews/reviews.html
+  data = save_as_rds(savedir, datafiles)
+  stopwords <- read.csv(file=stopwordfile, head=FALSE)
+  fit = fit_model(savedir, data, stopwords)
+  build_v11n(savedir, fit)
 
-run_lda_and_ldavis <- function(datafile, stopwordfile)
-{
-  t0 = proc.time()
-  message(sprintf('execution starts at %.3fms', t0[1]))
-  load(datafile, verbose=TRUE) # this creates the tafaseer object
-  message('loading stopwords...')
-    stopwords <- read.csv(file=stopwordfile, head=FALSE)
-  message(sprintf('%.2f: cleaning the corpus...', t(t0)))
-    tafaseer = clean_arabic_string(tafaseer)
-  # going by http://cpsievert.github.io/ldavis/reviews/reviews.html from here
-  message(sprintf('%.2f: tokenizing...', t(t0)))
-    doc.list = strsplit(tafaseer, '[[:space:]]+')
-    message('computing table of terms...')
-    term.table = table(unlist(doc.list))
-    term.table = sort(term.table, decreasing=TRUE)
-  message('removing superfluous terms...')
-    del = names(term.table) %in% stopwords | term.table < 5
-    term.table = term.table[!del]
-    vocab = names(term.table)
-  message('converting to lda input format...')
-    get.terms = function(x) {
-      index = match(x, vocab)
-      index = index[!is.na(index)]
-      rbind(as.integer(index - 1), as.integer(rep(1, length(index))))}
-    documents = lapply(doc.list, get.terms)
-  message(sprintf('%.2f: calculating statistics...', t(t0)))
-    D = length(documents) # number of documents
-    W = length(vocab)     # number of terms in the vocabulary
-    doc.length = sapply(documents, function(x) sum(x[2, ])) # number of tokens per document
-    N = sum(doc.length)   # total number of tokens in the data
-    term.frequency = as.integer(term.table) # frequencies of terms in the corpus
-  message(sprintf('D=%s\nW=%s\nN=%s\ndoc.length=%s\nterm.frequency=%s',
-    D, W, N,
-    doc.length,
-    term.frequency))
-  message(sprintf('%.2f: fitting the model...', t(t0)))
-    K = 20
-    G = 5000
-    alpha = 0.02
-    eta = 0.02
-    set.seed(357) # where do they take this from??
-    fit <- lda.collapsed.gibbs.sampler(
-      documents=documents,
-      K=K,
-      vocab=vocab,
-      num.iterations=G,
-      alpha=alpha,
-      eta=eta,
-      initial=NULL,
-      burnin=0,
-      compute.log.likelihood=TRUE)
-  message(sprintf('%.2f: visualizing...', t(t0)))
-    theta = t(apply(fit$document_sums + alpha, 2, function(x) x/sum(x)))
-    phi   = t(apply(t(fit$topics) + eta, 2, function(x) x/sum(x)))
-		tafaseer_data = list(
-			phi=phi,
-      theta=theta,
-      doc.length=doc.length,
-      vocab=vocab,
-      term.frequency=term.frequency)
-		json = createJSON(
-			phi=tafaseer_data$phi,
-      theta=tafaseer_data$theta,
-      doc.length=tafaseer_data$doc.length,
-      vocab=tafaseer_data$vocab,
-      term.frequency=tafaseer_data$term.frequency)
-  message(sprintf('%.2f: writing visualization data...', t(t0)))
-		serVis(json, out.dir='data_automated/ldavis', open.browser=FALSE)
+  message(sprintf('success after %.2f mins', (proc.time() - t0)[3] / 60))
 }
 
 # Read command-line arguments
 args = commandArgs(trailingOnly=TRUE)
 
 if (length(args)==0) {
-  # We just want this now
-  run_lda_and_ldavis(
-    '../../corpora/altafsir_com/selection/tafaseer.rds', 'data_automated/stopwords.csv')
-# The old thing, not used anymore now
-# message('Usage: ./topicmodel.R [list of files to process]')
+  message('Usage: ./topicmodel.R <save data dir> <stopword file> <list of text files>')
 } else {
-  lapply(run_lda, args)
+  run_tm(args[1], args[2], args[-c(1, 2)])
 }
