4a954b08de38fae20fcefbc139938beb5cabbe37
J. R. Schmid <jrs+git@weitnahbei.de>
2016-12-11 17:34:54 +0100

    This finally does it, I think.

--- tafaseer_group/02_analysis/wordcount_whole_corpus.sql
+++ tafaseer_group/02_analysis/wordcount_whole_corpus.sql
@@ -1,14 +1,15 @@
----
---- Helper table needed for second query
---- Get rid of it first if it already existed
----
+--
+-- Helper table needed for second query
+-- Get rid of it first if it already existed
+--
 DROP TABLE IF EXISTS wordcounts_by_author;
-CREATE TABLE wordcounts_by_author(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, author_name CHAR(255) UNIQUE NOT NULL, words INTEGER NOT NULL);
+CREATE TABLE wordcounts_by_author (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  author_name CHAR(255) UNIQUE NOT NULL, words INTEGER NOT NULL);
 
----
---- Determine wordcounts for everything the author wrote
---- Note this doesn't take into consideration duplicate pages!
----
+--
+-- Determine wordcounts for everything the author wrote
+-- Note this doesn't take into consideration duplicate pages!
+--
 INSERT INTO wordcounts_by_author (author_name, words)
 SELECT
   author_name,
@@ -22,9 +23,18 @@ FROM cts_units
 GROUP BY author_name
 ORDER BY words ASC;
 
----
---- Now determine wordcounts per page, if it's distinct from any other page
---- 
+--
+-- Another helper table to keep things flexible,
+-- giant queries are hard as they are...
+--
+DROP TABLE IF EXISTS wordcounts_by_unit;
+CREATE TABLE wordcounts_by_unit (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  text_hash CHAR(255), author_id CHAR(255), author_name CHAR(255), sura INTEGER, aaya INTEGER, words INTEGER, words_percentage FLOAT);
+
+--
+-- Now determine wordcounts per page
+-- 
+INSERT INTO wordcounts_by_unit (text_hash, author_id, author_name, sura, aaya, words, words_percentage)
 SELECT
   text_hash,
   --
@@ -36,10 +46,9 @@ SELECT
     U.author_name,
   --
   -- QUR'AN PASSAGE:
-  -- the way its usually given in literature
   --
-  (SUBSTR('000'||sura_id,-3,3) ||':'|| SUBSTR('000'||aaya_id,-3,3))
-    AS passage,
+  sura_id,
+  aaya_id,
   --
   -- AMOUNT OF WORDS SPENT ON EACH AAYA:
   -- we have to use a condition here to get a good value
@@ -58,8 +67,8 @@ SELECT
   -- need condition inside of it again so leaving that be as it
   -- would really push the running time of the query
   --
-  (W.words)
-    AS author_wordcount,
+  -- (W.words)
+  --   AS author_wordcount,
   --
   -- PERCENTAGE OF AAYA WORDCOUNT WRT AUTHOR WORDCOUNT:
   -- getting the ratio is just a simple matter of
@@ -78,4 +87,17 @@ SELECT
 FROM cts_units AS U
 JOIN wordcounts_by_author AS W
   ON U.author_name=W.author_name
-ORDER BY U.author_name ASC, percentage DESC, words_spent DESC, passage ASC;
+ORDER BY U.author_name ASC, percentage DESC, words_spent DESC, sura_id ASC, aaya_id ASC;
+
+--
+-- Finally ready to get real distinct wordcounts
+--
+SELECT
+  author_id, author_name, text_hash,
+  MIN(words) AS words,
+  sura,
+  MIN(aaya) AS aaya
+FROM
+  wordcounts_by_unit
+GROUP BY author_id, author_name, text_hash, sura 
+ORDER BY author_id ASC, words DESC, sura ASC, aaya ASC;
