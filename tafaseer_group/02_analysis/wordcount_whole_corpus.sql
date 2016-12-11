385ba8331a1a0c8b308f20a9b92d4d8bfb532ab2
J. R. Schmid <jrs+git@weitnahbei.de>
2016-12-11 16:17:18 +0100

    Adding helper query as well.

--- tafaseer_group/02_analysis/wordcount_whole_corpus.sql
+++ tafaseer_group/02_analysis/wordcount_whole_corpus.sql
@@ -1,3 +1,31 @@
+---
+--- Helper table needed for second query
+---
+CREATE TABLE wordcounts_by_author(id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, author_name CHAR(255) UNIQUE NOT NULL, words INTEGER NOT NULL);
+SELECT
+  author_name,
+  SUM((CASE WHEN LENGTH(text) >= 1
+        THEN
+          (LENGTH(text) - LENGTH(REPLACE(text, ' ', '')) + 1)
+        ELSE
+          (LENGTH(text) - LENGTH(REPLACE(text, ' ', '')))
+        END)) AS words
+FROM cts_units
+GROUP BY author_name
+ORDER BY words ASC;
+INSERT INTO wordcounts_by_author (author_name, words)
+SELECT
+  author_name,
+  SUM((CASE WHEN LENGTH(text) >= 1
+        THEN
+          (LENGTH(text) - LENGTH(REPLACE(text, ' ', '')) + 1)
+        ELSE
+          (LENGTH(text) - LENGTH(REPLACE(text, ' ', '')))
+        END)) AS words
+FROM cts_units
+GROUP BY author_name
+ORDER BY words ASC;
+
 SELECT
   --
   -- CATEGORY AND AUTHOR:
